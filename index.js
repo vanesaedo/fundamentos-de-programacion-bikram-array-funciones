7855e1b5572ecdcdd956e8d5358e6aac3303db34
Vanesa Edo <vanesa.en@gmail.com>
2024-02-12 20:52:54 +0100

    Subido hasta el ejercicio 7. No comprobados.

---
+++ index.js
@@ -0,0 +1,18 @@
+//--Iteraciones :crossed_swords: Pair Programming :crossed_swords:
+
+//Arrays: 1,2,3,4.
+
+const arrayVacio = [];
+const arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumerosPares = [0,2,4,6,8];
+const arrayBidimensional = array[[0, 1, 2], ['a', 'b', 'c']];
+
+//Funciones: 5, 6, 7, 8, 9.
+
+const suma = (a, b) => { return (a + b);};
+const potenciacion = (a, b) => { return (a^b);};
+const separarPalabras = ('hola mundo') => {separarPalabras.split(',');};
+
+
+
+
